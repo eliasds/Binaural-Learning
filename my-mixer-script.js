d7b7343418501c84f9b488d68a1c3f2e1e50b8f2
eliasds <eliasds@gmail.com>
2023-04-21 14:46:33 -0700

    Update my-mixer-script.js

--- my-mixer-script.js
+++ my-mixer-script.js
@@ -1,52 +1,96 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const fileA = document.getElementById('fileA');
-  const fileB = document.getElementById('fileB');
-  const mixButton = document.getElementById('mixButton');
-  const swapButton = document.getElementById('swapButton');
-  const playButton = document.getElementById('playButton');
-  const player = new Tone.Player();
-  const merger = new Tone.Merge();
-  const panner = new Tone.Panner();
+  const fileAInput = document.getElementById('fileA');
+const fileBInput = document.getElementById('fileB');
+const mixButton = document.getElementById('mixButton');
+const swapButton = document.getElementById('swapButton');
+const playButton = document.getElementById('playButton');
+const progressBar = document.getElementById('progressBar');
 
-  player.loop = true;
-  player.toDestination();
+let fileA, fileB, mixedAudio;
 
-  mixButton.addEventListener('click', function() {
-    // Load the two audio files into memory using Tone.js's Player
-    player.load(fileA.files[0].name, function() {
-      const left = new Tone.Gain();
-      const right = new Tone.Gain();
-      player.connect(left, 0, 0);
-      player.connect(right, 1, 0);
-      merger.connect(left);
-      merger.connect(right);
-      player.load(fileB.files[0].name, function() {
-        const left = new Tone.Gain();
-        const right = new Tone.Gain();
-        player.connect(left, 0, 1);
-        player.connect(right, 1, 1);
-        merger.connect(left);
-        merger.connect(right);
-        player.start();
-      });
-    });
+// Update progress bar value and label
+function updateProgress(progress, label) {
+  progressBar.value = progress;
+  progressBar.innerHTML = `${label}: ${progress}%`;
+}
+
+// Load and decode audio files
+function loadAudioFiles(fileA, fileB) {
+  return Promise.all([
+    Tone.loaded().then(() => Tone.decodeAudio(fileA)),
+    Tone.loaded().then(() => Tone.decodeAudio(fileB)),
+  ]);
+}
+
+// Mix audio files
+function mixAudio() {
+  updateProgress(0, 'Mixing');
+
+  const leftChannelA = mixedAudio.getChannelData(0);
+  const rightChannelB = mixedAudio.getChannelData(1);
+  const numSamples = mixedAudio.length;
+
+  for (let i = 0; i < numSamples; i++) {
+    leftChannelA[i] = mixedAudio.getChannelData(0)[i];
+    rightChannelB[i] = mixedAudio.getChannelData(1)[i];
+  }
+
+  updateProgress(100, 'Mixing');
+}
+
+// Swap left and right channels
+function swapChannels() {
+  const temp = mixedAudio.getChannelData(0);
+  mixedAudio.copyToChannel(mixedAudio.getChannelData(1), 0);
+  mixedAudio.copyToChannel(temp, 1);
+}
+
+// Play mixed audio
+function playMixedAudio() {
+  const player = new Tone.Player(mixedAudio).toDestination();
+  player.start();
+}
+
+// Event listeners
+fileAInput.addEventListener('change', (event) => {
+  fileA = event.target.files[0];
+  updateProgress(0, 'Uploading File A');
+  Tone.loaded().then(() => {
+    updateProgress(100, 'File A Uploaded');
   });
-
-  playButton.addEventListener('click', function() {
-    player.start();
-  });
-
-  swapButton.addEventListener('click', function() {
-    if (panner.pan.value === 0) {
-      panner.pan.value = 1;
-    } else {
-      panner.pan.value = 0;
-    }
-  });
-
-  panner.connect(Tone.Master);
-
-  merger.connect(panner);
-
 });
+
+fileBInput.addEventListener('change', (event) => {
+  fileB = event.target.files[0];
+  updateProgress(0, 'Uploading File B');
+  Tone.loaded().then(() => {
+    updateProgress(100, 'File B Uploaded');
+  });
+});
+
+mixButton.addEventListener('click', () => {
+  if (fileA && fileB) {
+    updateProgress(0, 'Mixing');
+    loadAudioFiles(fileA, fileB).then(([audioA, audioB]) => {
+      mixedAudio = new Tone.Buffer(audioA.numberOfChannels, audioA.length, audioA.sampleRate);
+      mixAudio();
+      updateProgress(100, 'Mixed');
+    });
+  }
+});
+
+swapButton.addEventListener('click', () => {
+  if (mixedAudio) {
+    swapChannels();
+  }
+});
+
+playButton.addEventListener('click', () => {
+  if (mixedAudio) {
+    playMixedAudio();
+  }
+});
+
+// Initialize progress bar
+updateProgress(0, 'Waiting');
