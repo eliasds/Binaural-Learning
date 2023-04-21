7b78c7aac87141f1e83c14916a690e4cfdab5b50
eliasds <eliasds@gmail.com>
2023-04-21 14:33:36 -0700

    Update audio-mixer.js

--- audio-mixer.js
+++ audio-mixer.js
@@ -1,45 +1,84 @@
 // Wait for the DOM to be loaded before attaching event listeners
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Get references to the HTML elements we'll be working with
   const fileA = document.getElementById('fileA');
   const fileB = document.getElementById('fileB');
   const mixButton = document.getElementById('mixButton');
   const swapButton = document.getElementById('swapButton');
+  const audioContext = new AudioContext();
   const audioPlayer = document.getElementById('audioPlayer');
+  let mixedBuffer;
 
-  // Attach an event listener to the mix button
   mixButton.addEventListener('click', function() {
-    
     // Load the two audio files into memory using an audio processing library
     // This is where you would use a library like Tone.js or the Web Audio API
     // to load and process the audio files
-    const audioA = ... // Load songA.mp3 into memory
-    const audioB = ... // Load songB.mp3 into memory
-    
-    // Combine the left and right channels of each audio file into a single mono channel
-    const monoA = ... // Combine the left and right channels of audioA
-    const monoB = ... // Combine the left and right channels of audioB
-    
-    // Mix the two mono channels into a single stereo output
-    const stereoOutput = ... // Mix monoA and monoB into a stereo output
-    
-    // Set the audio player's source to the stereo output and play it
-    audioPlayer.src = ... // Set the audio source to the stereo output
-    audioPlayer.play(); // Play the audio
-    
+    const readerA = new FileReader();
+    const readerB = new FileReader();
+    readerA.onload = function(event) {
+      audioContext.decodeAudioData(event.target.result, function(buffer) {
+        const sourceA = audioContext.createBufferSource();
+        sourceA.buffer = buffer;
+        const monoA = convertToMono(buffer);
+        if (mixedBuffer) {
+          mixBuffers(monoA, mixedBuffer);
+        } else {
+          mixedBuffer = monoA;
+        }
+      });
+    };
+    readerB.onload = function(event) {
+      audioContext.decodeAudioData(event.target.result, function(buffer) {
+        const sourceB = audioContext.createBufferSource();
+        sourceB.buffer = buffer;
+        const monoB = convertToMono(buffer);
+        if (mixedBuffer) {
+          mixBuffers(mixedBuffer, monoB);
+        } else {
+          mixedBuffer = monoB;
+        }
+      });
+    };
+    readerA.readAsArrayBuffer(fileA.files[0]);
+    readerB.readAsArrayBuffer(fileB.files[0]);
   });
 
-  // Attach an event listener to the swap button
   swapButton.addEventListener('click', function() {
-    
-    // Swap the left and right channels of the stereo output
-    const swappedOutput = ... // Swap the left and right channels of the stereo output
-    
-    // Set the audio player's source to the swapped output and play it
-    audioPlayer.src = ... // Set the audio source to the swapped output
-    audioPlayer.play(); // Play the audio
-    
+    if (!mixedBuffer) {
+      return;
+    }
+    const channelData = [];
+    channelData.push(mixedBuffer.getChannelData(1));
+    channelData.push(mixedBuffer.getChannelData(0));
+    const swappedBuffer = audioContext.createBuffer(2, mixedBuffer.length, mixedBuffer.sampleRate);
+    swappedBuffer.copyToChannel(channelData[0], 0);
+    swappedBuffer.copyToChannel(channelData[1], 1);
+    playBuffer(swappedBuffer);
   });
 
-});
+  function convertToMono(buffer) {
+    const monoBuffer = audioContext.createBuffer(1, buffer.length, buffer.sampleRate);
+    const channelData = buffer.getChannelData(0);
+    monoBuffer.copyToChannel(channelData, 0);
+    return monoBuffer;
+  }
+
+  function mixBuffers(bufferA, bufferB) {
+    const mixedLength = Math.max(bufferA.length, bufferB.length);
+    const mixedBuffer = audioContext.createBuffer(1, mixedLength, bufferA.sampleRate);
+    const channelData = [];
+    channelData.push(mixedBuffer.getChannelData(0));
+    channelData.push(bufferA.getChannelData(0));
+    channelData.push(bufferB.getChannelData(0));
+    for (let i = 0; i < mixedLength; i++) {
+      channelData[0][i] = (channelData[1][i] + channelData[2][i]) / 2;
+    }
+    playBuffer(mixedBuffer);
+  }
+
+  function playBuffer(buffer) {
+    const source = audioContext.createBufferSource();
+    source.buffer = buffer;
+    source.connect(audioContext.destination);
+    source
+
