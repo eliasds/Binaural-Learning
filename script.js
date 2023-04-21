690aa3c4db430f67aef95bef7ccea8d5a66485d9
eliasds <eliasds@gmail.com>
2023-04-21 16:48:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
-Promise.all([
-    Tone.loaded(),
-    new Tone.Player({
-        url: "SongA.mp3",
-        loop: true,
-        autostart: false
-    }).toDestination().connect(new Tone.Panner(1)).load(),
-    new Tone.Player({
-        url: "SongB.mp3",
-        loop: true,
-        autostart: false
-    }).toDestination().connect(new Tone.Panner(-1)).load()
-]).then(() => {
-    const songA = Tone.context._players.get("SongA.mp3");
-    const songB = Tone.context._players.get("SongB.mp3");
+const context = new Tone.Context();
+const songA = new Tone.Player({
+  url: "SongA.mp3",
+  loop: true,
+  autostart: false
+}).connect(new Tone.Panner(1)).sync().start(0);
+const songB = new Tone.Player({
+  url: "SongB.mp3",
+  loop: true,
+  autostart: false
+}).connect(new Tone.Panner(-1)).sync().start(0);
 
-    const playAButton = document.getElementById("playA");
-    const pauseAButton = document.getElementById("pauseA");
-    const playBButton = document.getElementById("playB");
-    const pauseBButton = document.getElementById("pauseB");
+const playAButton = document.getElementById("playA");
+const pauseAButton = document.getElementById("pauseA");
+const playBButton = document.getElementById("playB");
+const pauseBButton = document.getElementById("pauseB");
 
-    playAButton.addEventListener("click", function() {
-        console.log("Play Song A clicked");
-        songA.start();
-    });
+playAButton.addEventListener("click", function() {
+    console.log("Play Song A clicked");
+    songA.start(context.currentTime);
+});
 
-    pauseAButton.addEventListener("click", function() {
-        console.log("Pause Song A clicked");
-        songA.stop();
-    });
+pauseAButton.addEventListener("click", function() {
+    console.log("Pause Song A clicked");
+    songA.stop();
+});
 
-    playBButton.addEventListener("click", function() {
-        console.log("Play Song B clicked");
-        songB.start();
-    });
+playBButton.addEventListener("click", function() {
+    console.log("Play Song B clicked");
+    songB.start(context.currentTime);
+});
 
-    pauseBButton.addEventListener("click", function() {
-        console.log("Pause Song B clicked");
-        songB.stop();
-    });
+pauseBButton.addEventListener("click", function() {
+    console.log("Pause Song B clicked");
+    songB.stop();
 });
