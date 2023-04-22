52f3cc83857fbf6e9a0e57371b0284dad441fef4
eliasds <eliasds@gmail.com>
2023-04-22 15:55:27 -0700

    32

--- script.js
+++ script.js
@@ -27,12 +27,13 @@ const pannerA = new Tone.Panner(-1);
 // Connect the player to the panner, then to the mono, and then to the left output channel
 songA.connect(monoA);
 monoA.connect(pannerA);
-pannerA.connect(Tone.Destination.left);
+//pannerA.connect(Tone.Destination.left);
 const songB = new Tone.Player("SongB.mp3").toDestination();
-const monoB = new Tone.Mono().toDestination();
-songB.connect(monoB);
+//const monoB = new Tone.Mono().toDestination();
+//songB.connect(monoB);
 //const songA = document.getElementById("songA");
 //const songB = document.getElementById("songB");
+
 const playAButton = document.getElementById("playA");
 const pauseAButton = document.getElementById("pauseA");
 const playBButton = document.getElementById("playB");
