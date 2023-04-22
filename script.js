5dd5ba614a4318f37e34cfddeb831a5aa187b74a
eliasds <eliasds@gmail.com>
2023-04-21 22:57:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,37 @@ const player = new Tone.Player("SongA.mp3").toDestination();
 player.start();
 
 /*
+var songA = document.getElementById("songA");
+var songB = document.getElementById("songB");
+var playAButton = document.getElementById("playA");
+var pauseAButton = document.getElementById("pauseA");
+var playBButton = document.getElementById("playB");
+var pauseBButton = document.getElementById("pauseB");
+
+playAButton.addEventListener("click", function() {
+    console.log("Play Song A clicked");
+    songA.currentTime = 0;
+    songA.play();
+});
+
+pauseAButton.addEventListener("click", function() {
+    console.log("Pause Song A clicked");
+    songA.pause();
+});
+
+playBButton.addEventListener("click", function() {
+    console.log("Play Song B clicked");
+    songB.currentTime = 0;
+    songB.play();
+});
+
+pauseBButton.addEventListener("click", function() {
+    console.log("Pause Song B clicked");
+    songB.pause();
+});
+*/
+
+/*
 const songA = new Tone.Player({
   url: "SongA.mp3",
   loop: true,
