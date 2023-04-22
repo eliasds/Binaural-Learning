54b5933d3bb75a788c16f3976707abd6f581546b
eliasds <eliasds@gmail.com>
2023-04-22 16:31:25 -0700

    33

--- script.js
+++ script.js
@@ -20,7 +20,7 @@
 // Set the pan value to move the sound to the left or right channel
 // panner.pan.value = -1.0; // move the sound to the left
 
-
+/*
 const songA = new Tone.Player("SongA.mp3");
 const monoA = new Tone.Mono();
 const pannerA = new Tone.Panner(-1);
@@ -33,6 +33,41 @@ const songB = new Tone.Player("SongB.mp3").toDestination();
 //songB.connect(monoB);
 //const songA = document.getElementById("songA");
 //const songB = document.getElementById("songB");
+*/
+
+var wavesurfer = WaveSurfer.create({
+    container: '#demo' // this is the only required param
+});
+wavesurfer.load('SongA.mp3');
+var panner = wavesurfer.backend.ac.createPanner();
+wavesurfer.backend.setFilter(panner);
+var slider = document.querySelector('#panner-input');
+slider.addEventListener('input', function (e) {
+    var xDeg = parseInt(e.target.value);
+    var x = Math.sin(xDeg * (Math.PI / 180));
+    wavesurfer.panner.setPosition(x, 0, 0);
+});
+
+/*
+const input = wavesurfer.backend.ac.createGain();
+const splitter = wavesurfer.backend.ac.createChannelSplitter(2);
+const merger = wavesurfer.backend.ac.createChannelMerger(2);
+const leftGain = wavesurfer.backend.ac.createGain();
+const rightGain = wavesurfer.backend.ac.createGain();
+// This will make sure that a mono signal gets upmixed to stereo.
+// If you always have stereo sound you can remove it.
+input.channelCountMode = 'explicit';
+// It is only necessary to connect the right channel
+// because this is the one which needs optional parameters.
+splitter.connect(rightGain, 1);
+rightGain.connect(merger);
+rightGain.connect(merger, 0, 1);
+// Only the one connection which needs an optional parameter
+// needs to be done for the left channel
+leftGain.connect(merger, 0, 1);
+// wavesufer.js will connect everything else.
+wavesurfer.backend.setFilters([ input, splitter, leftGain, merger ]);
+*/
 
 const playAButton = document.getElementById("playA");
 const pauseAButton = document.getElementById("pauseA");
