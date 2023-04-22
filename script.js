79097f459efba2d7a3513ed2ce289d987737d893
eliasds <eliasds@gmail.com>
2023-04-22 15:10:22 -0700

    29

--- script.js
+++ script.js
@@ -1,38 +1,11 @@
 // Create a new oscillator and connect it to the default output destination
-// const osc = new Tone.Oscillator().toDestination();
+const osc = new Tone.Oscillator().toDestination();
 // Set the frequency and start the oscillator
-// osc.frequency.value = 440; // A4
+osc.frequency.value = 440; // A4
 // osc.start();
 
-// Create a new player with a stereo audio file
-// const player = new Tone.Player("SongB.mp3");
-// Create a new mono node and connect it to the player
-// const mono = new Tone.Mono().toDestination();
-// player.connect(mono);
-
-// Create a new panner and connect it to the default output destination
-// const panner = new Tone.Panner(0).toDestination();
-// Create a new oscillator and connect it to the panner
-// const osc = new Tone.Oscillator().connect(panner);
-// Set the frequency and start the oscillator
-// osc.frequency.value = 440; // A4
-// osc.start();
-// Set the pan value to move the sound to the left or right channel
-// panner.pan.value = -1.0; // move the sound to the left
-
-
-const songA = new Tone.Player("SongA.mp3");
-const pannerA = new Tone.Panner(-1);
-const monoA = new Tone.Mono();
-// Connect the player to the panner, then to the mono, and then to the left output channel
-songA.connect(pannerA);
-pannerA.connect(monoA);
-monoA.connect(Tone.Destination.left);
-
-songA.connect(monoA);
+const songA = new Tone.Player("SongA.mp3",{playbackRate: 4}).toDestination();
 const songB = new Tone.Player("SongB.mp3").toDestination();
-const monoB = new Tone.Mono().toDestination();
-songB.connect(monoB);
 //const songA = document.getElementById("songA");
 //const songB = document.getElementById("songB");
 const playAButton = document.getElementById("playA");
@@ -40,8 +13,17 @@ const pauseAButton = document.getElementById("pauseA");
 const playBButton = document.getElementById("playB");
 const pauseBButton = document.getElementById("pauseB");
 
+// Create a new player with a stereo audio file
+const player = new Tone.Player("SongB.mp3");
+// Create a new mono node and connect it to the player
+const mono = new Tone.Mono().toDestination();
+player.connect(mono);
+
+
 playAButton.addEventListener("click", function() {
     console.log("Play Song A clicked");
+//    songA.currentTime = 0;
+//    songA.play();
     songA.start();
 });
 
@@ -52,12 +34,14 @@ pauseAButton.addEventListener("click", function() {
 
 playBButton.addEventListener("click", function() {
     console.log("Play Song B clicked");
-    songB.start();
+//    songB.currentTime = 0;
+//    songB.play();
+    player.start();
 });
 
 pauseBButton.addEventListener("click", function() {
     console.log("Pause Song B clicked");
-    songB.stop();
+    player.stop();
 });
 
 /*
