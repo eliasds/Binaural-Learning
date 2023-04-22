3dd2aff8746427a8a5571d62b2e58535ca96585d
eliasds <eliasds@gmail.com>
2023-04-22 15:34:55 -0700

    30

--- toneworks_script.js
+++ toneworks_script.js
@@ -1,13 +1,13 @@
 // Create a new oscillator and connect it to the default output destination
-const osc = new Tone.Oscillator().toDestination();
+// const osc = new Tone.Oscillator().toDestination();
 // Set the frequency and start the oscillator
-osc.frequency.value = 440; // A4
+// osc.frequency.value = 440; // A4
 // osc.start();
 
 const songA = new Tone.Player("SongA.mp3").toDestination();
 const songB = new Tone.Player("SongB.mp3").toDestination();
 const monoB = new Tone.Mono().toDestination();
-songB.connect(mono);
+songB.connect(monoB);
 //const songA = document.getElementById("songA");
 //const songB = document.getElementById("songB");
 const playAButton = document.getElementById("playA");
@@ -16,10 +16,10 @@ const playBButton = document.getElementById("playB");
 const pauseBButton = document.getElementById("pauseB");
 
 // Create a new player with a stereo audio file
-const player = new Tone.Player("SongB.mp3");
+//const player = new Tone.Player("SongB.mp3");
 // Create a new mono node and connect it to the player
-const mono = new Tone.Mono().toDestination();
-player.connect(mono);
+//const mono = new Tone.Mono().toDestination();
+//player.connect(mono);
 
 
 playAButton.addEventListener("click", function() {
