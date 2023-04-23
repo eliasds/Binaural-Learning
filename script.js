874388d3c9b611f098550cd81a203fa8b04b6da4
eliasds <eliasds@gmail.com>
2023-04-22 19:23:05 -0700

    44

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myConstant = "Hello, world! - 43"; // The constant value to be passed to HTML
+const myConstant = "Hello, world! - 44"; // The constant value to be passed to HTML
 const myParagraph = document.getElementById("my-paragraph");
 myParagraph.textContent = myConstant; // Pass the constant value to the HTML element
 
@@ -25,18 +25,33 @@ myParagraph.textContent = myConstant; // Pass the constant value to the HTML ele
 // Set the pan value to move the sound to the left or right channel
 // panner.pan.value = -1.0; // move the sound to the left
 
-const songA = new Tone.Player("SongA.mp3",{playbackRate: 4}).toDestination();
-//const songA = new Tone.Player("SongA.mp3");
+
+// Create a new Player and load the audio file
+//const songA = new Tone.Player("SongA.mp3",{playbackRate: 4}).toDestination();
+
+// Create a new Player and load the audio file
+const songA = new Tone.Player("SongA.mp3");
+// Create a new GainNode
+const gainA = new Tone.Gain();
+// Create a new Mono node
 const monoA = new Tone.Mono();
-//const pannerA = new Tone.Panner(-1);
-const pannerA = new Tone.Panner(-1).toDestination();
-// Connect the player to the panner, then to the mono, and then to the left output channel
-songA.connect(monoA);
-//monoA.connect(pannerA);
+// Create a new Panner node with position set to left
+const pannerA = new Tone.Panner(-1);
+//const pannerA = new Tone.Panner(-1).toDestination();
+// Connect the audio file to the GainNode
+songA.connect(gainA);
+// Connect the GainNode to the Mono node
+gainA.connect(monoA);
+// Connect the Mono node to the Panner node
 monoA.connect(pannerA);
 // Connect the audio signal to the final output destination using Tone.toDestination()
 //pannerA.connect(Tone.toDestination());
-//pannerA.connect(Tone.Destination.left);
+// Connect the Panner node to the destination node
+//pannerA.connect(Tone.Destination);
+// Connect the Panner node to the left channel of the destination node using Tone.Destination.Left
+pannerA.connect(Tone.Destination.left);
+
+
 //const songB = new Tone.Player("SongB.mp3").toDestination();
 const songB = new Tone.Player("SongB.mp3");
 const monoB = new Tone.Mono().toDestination();
